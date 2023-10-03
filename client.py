288363de734425cb07b8a1174e75370af8ed9caf
Diego <diego.cast047@gmail.com>
2023-10-03 12:11:42 -0600

    DM´s added

--- client.py
+++ client.py
@@ -1,26 +1,13 @@
 import socket   
 import threading
-import time
 
 username = input("Enter your username: ")
 
-host = '127.0.0.1'
+host = '148.220.209.65'
 port = 55555
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect((host, port))
-
-
-def receive_user_list():#recibir lista de clientes conectados
-    while True:
-        try:
-            message = client.recv(1024).decode('utf-8')
-            if message.startswith("ChatBot: Usuarios conectados:"):
-                print(message)
-            else:
-                print(message)
-        except:
-            break
 
 def receive_messages():
     while True:
@@ -32,21 +19,14 @@ def receive_messages():
             else:
                 print(message)
         except:
-            print("An error Ocurred")
-            client.close
+            print("An error occurred")
+            client.close()
             break
 
 def write_messages():
     while True:
         message = input('')
-        client.send(f"{username}: {message}".encode("utf-8"))
-
-def disconnect():
-    client.send("QUIT".encode("utf-8"))
-    client.close()
-
-user_list_thread = threading.Thread(target=receive_user_list)
-user_list_thread.start()
+        client.send(message.encode('utf-8'))
 
 receive_thread = threading.Thread(target=receive_messages)
 receive_thread.start()
--- server.py
+++ server.py
@@ -1,8 +1,7 @@
 import socket   
 import threading
-import time
 
-host = '127.0.0.1'
+host = '0.0.0.0'
 port = 55555
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -11,28 +10,25 @@ server.bind((host, port))
 server.listen()
 print(f"Server running on {host}:{port}")
 
-
 clients = []
 usernames = []
-
 
 def broadcast(message, _client):
     for client in clients:
         if client != _client:
             client.send(message)
 
-
-def send_users_list(): #enviar lista de clientes conectados cada 30 segundos
-    while True:
-        time.sleep(30)
-        user_list = ", ".join(usernames)
-        broadcast(f"ChatBot: Usuarios conectados: {user_list}".encode('utf-8'), None)
-
 def handle_messages(client):
     while True:
         try:
             message = client.recv(1024)
-            broadcast(message, client)
+            if message.decode('utf-8').startswith("@"):
+                recipient_username, private_message = message.decode('utf-8').split(' ', 1)
+                recipient_index = usernames.index(recipient_username[1:])
+                recipient_client = clients[recipient_index]
+                recipient_client.send(f"(Private) {usernames[clients.index(client)]}: {private_message}".encode('utf-8'))
+            else:
+                broadcast(message, client)
         except:
             index = clients.index(client)
             username = usernames[index]
@@ -41,7 +37,6 @@ def handle_messages(client):
             usernames.remove(username)
             client.close()
             break
-
 
 def receive_connections():
     while True:
@@ -62,19 +57,4 @@ def receive_connections():
         thread = threading.Thread(target=handle_messages, args=(client,))
         thread.start()
 
-def handle_client_disconnect(client):
-    index = clients.index(client)
-    username = usernames[index]
-
-    broadcast(f"ChatBot: {username} disconnected".encode('utf-8'), client)
-    clients.remove(client)
-    usernames.remove(username)
-
-    print(f"{username} disconnected")
-
-
-user_list_thread = threading.Thread(target=send_users_list)
-user_list_thread.start()
-
 receive_connections()
-
